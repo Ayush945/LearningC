0a09bc4ccab54ce6b78a0922c2f690f18c887cf7
Ayush <ayushthapa0010@gmail.com>
2025-03-13 16:29:23 +0545

    part one

---
+++ Part1/Constant.C
@@ -0,0 +1,21 @@
+#define AGELIMIT 21
+#define MYNAME "Ayush Thapa"
+#define PI 3.14159
+
+
+#define KIDS 3
+#define FAMILY "The Royals"
+#define MORTAGE_RATE 5.15
+
+
+// int main(){
+//     int employeeAge=22;
+//     //PI=25; NOT ALLOWED
+//     if(employeeAge<AGELIMIT){
+//         printf("You are underage");
+//     }
+//     else{
+//         printf("Entry granted");
+//     }
+//     return 0;
+// }
