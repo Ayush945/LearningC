075b23f1354b64fadc0af1f15f1f79f2d188410d
Ayush <ayushthapa0010@gmail.com>
2025-03-22 12:02:51 +0545

    pointer practice

---
+++ Basic1/part9/pointerexercise.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int num=1;
+    int *pNum;
+
+    pNum=&num;
+    printf("The address of number is: %p.\n",pNum);
+    *pNum=10;
+    printf("The number is: %d.\n",num);
+    printf("The address of number is: %p.\n",pNum);
+    return 0;
+}
