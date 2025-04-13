55fed5567cccddfcf68f0844568a776c4424a9d3
Ayush <ayushthapa0010@gmail.com>
2025-04-13 12:20:14 +0545

    dynamic and struct

--- Basic1/Part14/Easy.c
+++ Basic1/Part14/Easy.c
@@ -15,13 +15,29 @@ int main(){
     *pY=temp;
 
     //Question 3
-    printf("Address of X = %p",pX);
+    printf("Address of X = %p",(void*)pX);
 
     /* Strings 
-        Question --- 11
+        Question --- 11,12,13
     */ 
-    
+    char name[]="Ayush";
+    char container[6]="";
 
+    int size = sizeof(name)/sizeof(char);
+    printf("\nThe size is: %d\n",size);
+    for(int i=0; i<size;i++){
+        container[i]=name[i];
+        
+    }
+    printf("\nTotal number =%d\n",size-1);
+    
+    for(int i=0; i<size;i++){
+        printf("%c",container[i]);
+    }
+    printf("\n");
+    for(int i=(size-2);i>=0;i--){
+        printf("%c",container[i]);
+    }
 
 
     return 0;
--- Basic1/Part14/Easy1.c
+++ Basic1/Part14/Easy1.c
@@ -1,5 +1,91 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int IncreaseNumber(int* num){
+
+    *num=*num+1;
+    return *num;
+}
+
+struct Student{
+    char name[50];
+    int age;
+    float marks;
+};
 
 int main(){
+
+    //Pointer question 14,15,16
+
+    int num =2;
+    int* pNum=&num;
+    int newNum=IncreaseNumber(pNum);
+    printf("\nThe new number is = %d\n",newNum);
+    
+    int arr[5]={2,5,10,14,25};
+    printf("The values of array are : %d,%d,%d,%d,%d",*arr,*(arr+1),*(arr+2),*(arr+3),*(arr+4));
+    printf("\n");
+    int temp;
+    temp=*arr;
+    
+    *arr=*(arr+4);
+    *(arr+4)=temp;
+
+    temp=*(arr+1);
+    *(arr+1)=*(arr+3);
+    *(arr+3)=temp;
+
+    printf("The new array are : %d,%d,%d,%d,%d",*arr,*(arr+1),*(arr+2),*(arr+3),*(arr+4));
+    
+    //Dynamic Memory Allocation --- question 17,18
+    // int* newArray= (int*) malloc(2*sizeof(int)); //stores heap address in newArray
+
+    
+    // printf("\n");
+    // printf("Enter a number:");
+    // scanf("%d",newArray);
+    // printf("\n");
+    // printf("Enter a number:");
+    // scanf("%d",newArray+1);
+    
+    // printf("The sum is : %d",*(newArray)+*(newArray+1));
+    // printf("\n");
+
+    // int* rellarr=(int*)realloc(newArray,4*sizeof(int));
+
+    // free(rellarr);
+
+    // Dynamic Memory Allocation --- question 19
+
+    int i,n=2;
+
+    //Allocating memory for n students of type struct pointer
+    struct Student* students =(struct Student*)malloc(n*(sizeof(struct Student)));
+
+    //Safety if malloc doesnt work and allocate address
+    if(students==NULL){
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
+    for(i=0;i<n;i++){
+        printf("\nEnter details for student %d:\n",i+1);
+        printf("Name: ");
+        scanf(" %s",students[i].name);
+        printf("Age: ");
+        scanf("%d",&students[i].age);
+        printf("Marks: ");
+        scanf("%f",&students[i].marks);
+    }
+
+    printf("\n\nStudent Details: \n");
+    for(i=0;i<n;i++){
+        printf("Student %d: Name = %s, Age = %d, Marks = %.2f\n",i+1,students[i].name,students[i].age,students[i].marks);
+    }
+
+
+    free(students);
+    
+
     return 0;
 }
