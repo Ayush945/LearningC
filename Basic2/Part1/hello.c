99a59463a2efe1906bef214ba1d26572afd1c820
Ayush <ayushthapa0010@gmail.com>
2025-04-26 10:35:01 +0545

    DS

--- Basic2/Part1/hello.c
+++ Basic2/Part1/hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+int StudentId(){
+    int studentId;
+    printf("Enter studentId: ");
+    scanf("%d",&studentId);
+    return studentId;
+}
 int main(){
 
     printf("Hello, I am back, now will be start of next season of C");
+    printf("\nStudying DS Now\n");
+    printf("Student ID : %d ", StudentId());
 }
