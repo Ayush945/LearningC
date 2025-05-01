0f6ae59bf43ba5c51fcf63c906ec431977bc8ea5
Ayush <ayushthapa0010@gmail.com>
2025-05-01 12:46:11 +0545

    pointer array

---
+++ Basic2/Part2/pointerArray.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+
+int main(){
+    char name[20];
+    char* pName=name; // Pointer to beginning of the array
+    char ch;
+
+    printf("Enter your name: ");
+    
+    while((ch=getchar())!='\n' && ch!=EOF){
+        *pName=ch;
+        pName++;
+    }
+    *pName='\0';
+    printf("\nThe name is: %s\n",name);
+    return 0;
+}
