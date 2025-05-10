3fb8c2db4991448ebf1eac126119f13d166c3036
Ayush <ayushthapa0010@gmail.com>
2025-05-10 13:38:28 +0545

    input value

--- Basic2/Part2/pointer.c
+++ Basic2/Part2/pointer.c
@@ -2,10 +2,12 @@
 
 int main(){
 
-    int x=50;
+    int x;
     int* pX=&x;
     int** ptr=&pX;
 
+    printf("Enter the value of x: ");
+    scanf("%d",&x);
     printf("Value of x is : %d",*(*ptr));
 
 }
