1a598cd8383517558435f58a0b85a7f845708394
Ayush <ayushthapa0010@gmail.com>
2025-05-11 18:03:05 +0545

    some array defination and printing

--- Basic2/Part2/Datastructure.c
+++ Basic2/Part2/Datastructure.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#define MAX_SIZE 10
+
 
 int main(){
-
+    int number =10;
     //Time to start data structure in C
+    int array[MAX_SIZE];
+    
+    for(int i=0; i<MAX_SIZE;i++){
+
+        array[i]=number;
+        number=number+10;
+    }
+
+    for(int i=0; i<MAX_SIZE;i++){
+
+        printf("\nPrinting integers: %d",array[i]);
+    }
+    
     return 0;
 }
