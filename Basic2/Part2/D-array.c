f51c63f220f209348908ca9cfe1c0cd316fe794e
Ayush <ayushthapa0010@gmail.com>
2025-05-13 07:26:56 +0545

    D array

---
+++ Basic2/Part2/D-array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int array [2][2]  ={{9,14},{24,15}};
+
+    printf("\nThe first element:  %d",*(*(array)));
+    printf("\nThe second element: %d",*(*(array)+1));
+    printf("\nThe third element:  %d",*(*(array+1)));
+    printf("\nThe fourth element: %d",*(*(array+1)+1));
+
+
+    return 0;
+}
