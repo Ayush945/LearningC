0f56b79837873638251f52d423d9e892658b797d
Ayush <ayushthapa0010@gmail.com>
2025-06-09 14:23:14 +0545

    test

---
+++ Basic2/Part3/Test.c
@@ -0,0 +1,13 @@
+// Dynamically create and store a 2D matrix.
+
+// Insert matrix elements into a linked list.
+
+// Use recursion to print the list in reverse.
+
+// Clean up memory.
+
+#include <stdio.h>
+int main(){
+
+    return 0;
+}
