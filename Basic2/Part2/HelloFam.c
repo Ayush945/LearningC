2711fc936facf3c48a749f6fb818e9f546ce94ec
Ayush <ayushthapa0010@gmail.com>
2025-06-16 12:55:57 +0545

    more

--- Basic2/Part2/HelloFam.c
+++ Basic2/Part2/HelloFam.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main(){
 
     printf("Hello there, my friend");
-    printf("Should start with implementation of linked list");
-    prinf("Understanding Linked List.");
+    printf("\nShould start with implementation of linked list");
+    printf("\nUnderstanding Linked List.");
+
+    //Access direct memory from C.
+    //int *ptr=(int *)0x000009BDCDFFC38; mostly no allowed so used dynamic memory
+
+    int* ptr=(int*) malloc(sizeof(int));
+    if(ptr==NULL){
+        printf("Memory not allocated");
+    }
+
+    *ptr = 42;
+
+    printf("\nThe location of pointer: %p",ptr);
+    printf("\n");
+    printf("The value of pointer: %d",*ptr);
+
+    free(ptr);
     return 0;
 }
