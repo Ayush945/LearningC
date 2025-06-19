2af3ded14a866d6e5970a24a3432bce399408209
Ayush <ayushthapa0010@gmail.com>
2025-06-19 14:02:13 +0545

    linked list insert at beginning

---
+++ Basic2/Part4/LinkedListInsert.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//Node
+struct Node{
+    int data;
+    struct Node* next;
+};
+
+//Main pointer to node which stores the first node of linked list---helps in traversal of linked list
+struct Node* head;
+
+
+//Insert element on beginning of linked list.
+void Insert(int x){
+
+    struct Node* temp= (struct Node*)malloc(sizeof(struct Node));
+    temp->data=x;
+    temp->next=head;
+    head=temp;  
+}
+
+void Print(){
+
+    struct Node* temp=head;
+    printf("List is: ");
+    while (temp!=NULL){
+        printf("%d",temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+}
+
+
+int main(){
+    head=NULL; // empty
+    printf("How many numbers?\n");
+    int n,i,x;
+    scanf("%d",&n);
+    for (i=0;i<n;i++){
+        printf("Enter the number \n");
+        scanf("%d",&x);
+        Insert(x);
+        Print();
+    }
+
+
+    return 0;
+}
---
+++ Basic2/Part4/LinkedListInsert1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node* next;
+};
+
+void InsertAtFirst(struct Node** head, int x){
+    struct Node* temp=(struct Node*)malloc(sizeof(struct Node));
+    temp->data=x;
+    temp->next=*head;
+    *head=temp;
+}
+
+void PrintData(struct Node* head){
+    printf("\n");
+    struct Node* temp=head;
+    printf("List is: ");
+    while(temp!=NULL){
+        printf("%d",temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+}
+
+int main(){
+    struct Node* head=NULL;
+    int n,i,x;
+    printf("How many numbers: ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("\nEnter the number: ");
+        scanf("%d",&x);
+        InsertAtFirst(&head,x);
+        PrintData(head);
+    }
+
+    return 0;
+}
