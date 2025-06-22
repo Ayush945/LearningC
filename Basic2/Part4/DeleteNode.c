d0dc2698844a31cdac8d8ec9ed094c4e4abd3b71
Ayush <ayushthapa0010@gmail.com>
2025-06-22 21:15:37 +0545

    not complete

---
+++ Basic2/Part4/DeleteNode.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node* next;
+};
+
+struct Node* head;
+
+void Insert(int data){ //insert an integer at end of list
+    struct Node* temp=(struct Node*)malloc(sizeof(struct Node));
+    temp->data=data;
+    temp->next=NULL;
+    if(head==NULL){
+        head=temp;
+        return;
+    }
+    struct Node* temp1=head;
+    while(temp1->next!=NULL){
+        temp1=temp1->next;
+    }
+    temp1->next=temp;
+
+}
+
+void Print(){//print all elements in the list
+    struct Node* temp=head;
+    printf("List is: ");
+    while(temp!=0){
+        printf("%d",temp->data);
+        temp=temp->next;
+    }
+
+    printf("\n");
+}
+
+void Delete(int n){//delete node at positio n
+    printf("%d",n);
+}
+
+int main(){
+
+    head=NULL;
+    Insert(1);
+    Insert(2);
+    Insert(3);
+    Insert(4);
+    Insert(5); //List is 1,2,3,4,5
+    Print();
+    printf("\n");
+    int n;
+    printf("Enter a position: ");
+    scanf("%d",&n);
+    printf("\n");
+    //Delete(n);
+    Print();
+
+    return 0;
+}
