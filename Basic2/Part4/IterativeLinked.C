98b4b824ca78876358976011dfefec8c07c3a08d
Ayush <ayushthapa0010@gmail.com>
2025-06-25 09:25:18 +0545

    reverse list

--- Basic2/Part4/IterativeLinked.C
+++ Basic2/Part4/IterativeLinked.C
@@ -1,7 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+struct Node{
+    int data;
+    struct Node* next;
+};
 
+struct Node* head;
+
+void Insert(int data){
+    struct Node* temp=(struct Node*)malloc(sizeof(struct Node));
+    temp->data=data;
+    temp->next=head;
+    head=temp;
+}
+
+void Print(){
+    struct Node* temp=head;
+    printf("List is: ");
+    while(temp!=NULL){
+        printf("%d",temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+}
+
+void PrintReverse(){
+    struct Node* temp=head;
+}
+int main(){
+    head=NULL;
+    Insert(2);
+    Insert(3);
+    Insert(4);
+    Insert(5);
+    Insert(6); //List is 6,5,4,3,2
+    Print(); //List is 6,5,4,3,2
+    printf("\n");
+    PrintReverse(); //List is 2,3,4,5,6
     return 0;
 }
