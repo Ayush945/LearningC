7b54a4478b1ac421f437c7e30f92e1e475046a66
Ayush <ayushthapa0010@gmail.com>
2025-06-30 15:30:33 +0545

    more

--- C/Data_Structure/LinkedList/LinkedLIstRecursion.c
+++ C/Data_Structure/LinkedList/LinkedLIstRecursion.c
@@ -6,6 +6,23 @@ struct Node{
     struct Node* next;
 };
 
+//Insert from head;
+void Insert(int x){
+    struct Node* temp=(struct Node*)malloc(sizeof(struct Node));
+    temp->data=x;
+    temp->data=head;
+    head=temp;    
+}
+
+void Print(){
+    struct Node* temp;
+    printf("List is: ");
+    while(temp!=NULL){
+        printf("%d",temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+}
 
 struct Node* head;
 int main(){
