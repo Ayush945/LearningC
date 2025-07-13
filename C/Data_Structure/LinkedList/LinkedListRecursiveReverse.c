85f8bff70458054f019c8aa88db60e43323bb325
Ayush <ayushthapa0010@gmail.com>
2025-07-13 16:25:05 +0545

    recursion linked list reverse

---
+++ C/Data_Structure/LinkedList/LinkedListRecursiveReverse.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int data;
+    struct Node* next;
+};
+
+
+struct Node* head;
+
+void Insert(int n){
+
+    struct Node* temp=(struct Node*)malloc(sizeof(struct Node));
+    temp->data=n;
+    temp->next=NULL;
+
+    if(head==NULL){
+        head=temp;
+        return;
+    }
+    
+    struct Node* temp1=head;
+    while(temp1->next!=NULL){
+        temp1=temp1->next;
+    }
+    temp1->next = temp;
+}
+
+void ReverseLinkedListRecursion(){
+
+}
+
+void PrintLinkedList(){
+
+    struct Node* temp=head;
+    while(temp!=NULL){
+        printf("%d",temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+}
+
+
+int main(){
+    head=NULL;
+    Insert(1);
+    Insert(2);
+    Insert(3);
+    Insert(4);
+    Insert(5);
+    PrintLinkedList();
+    //ReverseLinkedListRecursion();
+    PrintLinkedList();
+    
+    return 0;
+}
