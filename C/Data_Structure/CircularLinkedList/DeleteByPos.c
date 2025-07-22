124d5351357d6b5f1f2b9e19921673a58bf5b3a1
Ayush <ayushthapa0010@gmail.com>
2025-07-22 07:19:47 +0545

    circular linked list

--- C/Data_Structure/CircularLinkedList/DeleteByPos.c
+++ C/Data_Structure/CircularLinkedList/DeleteByPos.c
@@ -38,15 +38,35 @@ void InsertAtHead(int num){
 }
 
 void AddAtPos(int data,int pos){
-
+    struct Node* node=GetNewNode(data);
+    struct Node* temp=head;
+    for(int i=0;i<pos-2;i++){
+        temp=temp->next;
+    }
+    node->prev=temp;
+    node->next=temp->next;
+    struct Node* temp2=temp->next;
+    temp->next=node;
+    temp2->prev=node;
 }
 
 void ReplaceByPos(int data,int pos){
-
+    struct Node* temp=head;
+    for(int i=0;i<pos-1;i++){
+        temp=temp->next;
+    }
+    temp->data=data;
 }
 
 void DeleteByPos(int pos){
-
+    struct Node* temp=head;
+    for(int i=0;i<pos-1;i++){
+        temp=temp->next;
+    }
+    struct Node* temp2=temp;
+    temp->prev->next=temp->next;
+    temp->next->prev=temp->prev;
+    free(temp2);
 }
 
 void Print(){
@@ -62,18 +82,22 @@ void Print(){
 
 int main(){
     head=NULL;
-    InsertAtHead(5);
-    InsertAtHead(4);
-    InsertAtHead(3);
-    InsertAtHead(2);
-    InsertAtHead(1);
-    Print();
-    AddAtPos(9,3);
-    Print();
-    ReplaceByPos(7,2);
-    Print();
-    DeleteByPos(3);
-    Print();
+    InsertAtHead(5); // List: 5
+    InsertAtHead(4); // List: 4 5
+    InsertAtHead(3); // List: 3 4 5
+    InsertAtHead(2); // List: 2 3 4 5
+    InsertAtHead(1); // List: 1 2 3 4 5
+    Print();         // => List is: 12345
+
+    AddAtPos(9,3);   // List: 1 2 9 3 4 5
+    Print();         // => List is: 129345
+
+    ReplaceByPos(7,2); // 2nd element becomes 7 -> List: 1 7 9 3 4 5
+    Print();           // => List is: 179345
+
+    DeleteByPos(3);    // Delete 3rd element (9) -> List: 1 7 3 4 5
+    Print();           // => List is: 17345
+
     
     return 0;
 }
