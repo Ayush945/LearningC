45a02816887a5a594f8fc704bdaadb350b496506
Ayush <ayushthapa0010@gmail.com>
2025-07-29 14:03:21 +0545

    complete binary tree with array

--- C/Data_Structure/Trees/BinaryTreeTry.c
+++ C/Data_Structure/Trees/BinaryTreeTry.c
@@ -5,7 +5,7 @@ struct Node{
     int data;
     struct Node* left;
     struct Node* right;
-}
+};
 
 struct Node* root=NULL;
 
@@ -16,6 +16,30 @@ void OrderedCreateBinaryTree(int data){
     temp->right=NULL;
 }
 
-int main(){
+
+// Lets try to store complete binary tree with Array
+int main() {
+    int treeStr[7];
+    treeStr[0] = 2;
+    treeStr[1] = 4;
+    treeStr[2] = 1;
+    treeStr[3] = 5;
+    treeStr[4] = 8;
+    treeStr[5] = 7;
+    treeStr[6] = 9;
+
+    for (int i = 0; i < 7; i++) {
+        int left_child = 2 * i + 1;
+        int right_child = 2 * i + 2;
+
+        if (left_child < 7 && treeStr[left_child] != 0) {
+            printf("\n%d is left child of number %d.", treeStr[left_child], treeStr[i]);
+        }
+
+        if (right_child < 7 && treeStr[right_child] != 0) {
+            printf("\n%d is right child of number %d.", treeStr[right_child], treeStr[i]);
+        }
+    }
+
     return 0;
 }
