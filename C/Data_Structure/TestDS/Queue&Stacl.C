6d5c961c172e0a9507aa4373456a54044153d0fa
Ayush <ayushthapa0010@gmail.com>
2025-08-31 19:05:48 +0545

    stack and queue1

--- C/Data_Structure/TestDS/Queue&Stacl.C
+++ C/Data_Structure/TestDS/Queue&Stacl.C
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
-int main(){
+struct Node{
+    int data;
+    struct Node* next;
+};
 
+struct Node* head;
+
+void Pop(){
+
+}
+
+int Top(){
+    
+}
+
+int main(){
+    head==NULL;
     printf("Stack and Queue Practice");
     return 0;
 }
